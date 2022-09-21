9e9e5097ac451e5429f22db474695a4924199410
omara256 <omaraperker56@gamil.com>
2022-09-20 23:52:18 -0700

    Added Task 0 Updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# Create a file in /tmp
-
+# Creates a file in /tmp
 file { '/tmp/holberton':
-  ensure  => 'file',
+  ensure  => file,
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
